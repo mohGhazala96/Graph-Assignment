1797f411eca4bbb238ff97e3185c2a3f8fa5ab92
mohGhazala96 <mohamed.aboughazala@student.guc.edu.eg>
2018-11-24 20:01:59 +0200

    finished bfs

--- src/Graph.java
+++ src/Graph.java
@@ -1,34 +1,31 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.Collections;
 import java.util.Set;
 import java.util.Vector;
 
-public class Graph {
+public class Graph implements Visitor {
 
 	private HashMap<String, Vertex> vertices;
 	private HashMap<String, Edge> edges;
+	HashMap<String, Boolean> visitedEdges;
+	HashMap<String, Boolean> visitedVertices;
 
 	public Graph() {
 		super();
-		
+
 		this.vertices = new HashMap<String, Vertex>();
 	}
-	
-	
-	public void insertVertex(String strUniqueID,
-			String strData,
-			int
-			nX,
-			int
-			nY) throws GraphException {
+
+	public void insertVertex(String strUniqueID, String strData, int nX, int nY) throws GraphException {
 		if (vertices.containsKey(strUniqueID)) {
-			throw new GraphException("ID already exists .. and it's called unique you idiot");
+			throw new GraphException("ID already exists");
 		}
 
 		Vertex vertex = new Vertex(new StringBuffer(strUniqueID), new StringBuffer(strData), nX, nY);
-		
+
 		vertices.put(strUniqueID, vertex);
 	}
 
@@ -55,37 +52,38 @@ public class Graph {
 		edges.get(strEdgeUniqueID).removeEdge();
 		edges.remove(strEdgeUniqueID);
 	}
-	
-	public Vector<Edge> incidentEdges(String strVertexUniqueID)
-			throws GraphException {
+
+	public Vector<Edge> incidentEdges(String strVertexUniqueID) throws GraphException {
 		Vertex vertex = vertices.get(strVertexUniqueID);
-		
+
 		if (vertex == null) {
-			throw new GraphException("Vertex doesn't exist dude...");
+			throw new GraphException("Vertex doesn't exist.");
 		}
-		
+
 		return new Vector<Edge>(vertex._edges);
 	}
-	
+
 	public Vector<Vertex> vertices() throws GraphException {
 		Set verSet = this.vertices.entrySet();
-		
+
 		return new Vector<Vertex>(verSet);
 	}
-	
+
 	public Vector<Edge> edges() throws GraphException {
 		Set edgSet = this.edges.entrySet();
-		
+
 		return new Vector<Edge>(edgSet);
 	}
-	
-	public void removeVertex(String strVertexUniqueID) throws GraphException{
-		for(int i=0;i<vertices.get(strVertexUniqueID)._edges.size();i++){ 
+
+	public void removeVertex(String strVertexUniqueID) throws GraphException {
+		for (int i = 0; i < vertices.get(strVertexUniqueID)._edges.size(); i++) {
 			boolean found = false;
 			String idOfEdge = vertices.get(strVertexUniqueID)._edges.get(i)._strUniqueID.toString();
-			Vertex v = vertices.get(strVertexUniqueID)._edges.get(i).getConnectedVertex(vertices.get(strVertexUniqueID));
-			for(int j =0;j<v._edges.size() && !found ;i++){
-				if(v._edges.get(j).getConnectedVertex(v)._strUniqueID.toString().equals(vertices.get(strVertexUniqueID)._strUniqueID.toString())){
+			Vertex v = vertices.get(strVertexUniqueID)._edges.get(i)
+					.getConnectedVertex(vertices.get(strVertexUniqueID));
+			for (int j = 0; j < v._edges.size() && !found; i++) {
+				if (v._edges.get(j).getConnectedVertex(v)._strUniqueID.toString()
+						.equals(vertices.get(strVertexUniqueID)._strUniqueID.toString())) {
 					found = true;
 					v._edges.remove(v._edges.get(j));
 				}
@@ -95,13 +93,53 @@ public class Graph {
 		}
 		vertices.remove(strVertexUniqueID);
 	}
-	public Vertex[]endVertices(String strEdgeUniqueID) throws GraphException{
+
+	public Vertex[] endVertices(String strEdgeUniqueID) throws GraphException {
 		return edges.get(strEdgeUniqueID).get_vertices();
 	}
-	
-	public Vertex opposite(String strVertexUniqueID, String strEdgeUniqueID) throws GraphException{
+
+	public Vertex opposite(String strVertexUniqueID, String strEdgeUniqueID) throws GraphException {
 		Edge current = edges.get(strEdgeUniqueID);
 		return current.getConnectedVertex(vertices.get(strVertexUniqueID));
+
+	}
+	public  void visit(Vertex v) {
+		visitedVertices.put(v._strUniqueID.toString(), true);
+
+	}
+	public  void visit(Edge e) {
+		visitedEdges.put(e._strUniqueID.toString(), true);
+
+	}
+
+	public void bfs(String strStartVertexUniqueID, Visitor visitor) throws GraphException
+
+	{
+
+		visitedEdges = new HashMap<String, Boolean>();
+		visitedVertices = new HashMap<String, Boolean>();
 		
+		LinkedList<Vertex> queue = new LinkedList<Vertex>();
+		Vertex originalVertex = vertices.get(strStartVertexUniqueID);
+
+		this.visit(originalVertex);
+		queue.add(originalVertex);
+
+		while (queue.size() != 0) {
+			Vertex outVertex = queue.poll();
+			System.out.print(outVertex._strUniqueID + " -> ");
+			Iterator<Edge> edgesOfVertex = outVertex._edges.listIterator();
+			
+			while (edgesOfVertex.hasNext()) {
+				Edge currentEdge = edgesOfVertex.next();
+				this.visit(currentEdge);
+				Vertex nextVertex =currentEdge.get_vertices()[1];
+				if (!visitedVertices.get(nextVertex._strUniqueID.toString())) {
+					this.visit(nextVertex);
+					queue.add(nextVertex);
+				}
+			}
+		}
+
 	}
 }

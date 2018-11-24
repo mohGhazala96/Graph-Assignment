e758451137d5d0c661875056cd5451259d0bf61d
wessam611 <wessam611@live.com>
2018-11-24 15:33:58 +0200

    added insert vertex

--- src/Graph.java
+++ src/Graph.java
@@ -1,10 +1,29 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Graph {
 
-	private ArrayList<Vertex> vertices;
+	private HashMap<String, Vertex> vertices;
 
 	public Graph() {
 		super();
+		
+		this.vertices = new HashMap<String, Vertex>();
+	}
+	
+	
+	public void insertVertex(String strUniqueID,
+			String strData,
+			int
+			nX,
+			int
+			nY) throws GraphException {
+		if (vertices.containsKey(strUniqueID)) {
+			throw new GraphException("ID already exists .. and it's called unique you idiot");
+		}
+
+		Vertex vertex = new Vertex(new StringBuffer(strUniqueID), new StringBuffer(strData), nX, nY);
+		
+		vertices.put(strUniqueID, vertex);
 	}
 }
--- src/Vertex.java
+++ src/Vertex.java
@@ -5,7 +5,20 @@ public class Vertex {
 	protected StringBuffer _strUniqueID, _strData;
 	protected int _nX, _nY;
 	protected LinkedList<Edge> _edges;
+	
+	public Vertex(StringBuffer _strUniqueID, StringBuffer _strData, int _nX, int _nY) {
+		super();
+		this._strUniqueID = _strUniqueID;
+		this._strData = _strData;
+		this._nX = _nX;
+		this._nY = _nY;
+	}
 
+	public Vertex(StringBuffer _strUniqueID, StringBuffer _strData) {
+		new Vertex(_strUniqueID, _strData, 0, 0);
+	}
+
+	
 	// a unique id identifying vertex
 	// data associated with vertex
 	// Coordinates of vertex on some

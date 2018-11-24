4c7b3e9d70a377677dd4f6afaa76661ab5062aeb
mohGhazala96 <mohamed.aboughazala@student.guc.edu.eg>
2018-11-24 15:01:27 +0200

    Added insertEdge

--- src/Graph.java
+++ src/Graph.java
@@ -1,10 +1,20 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Graph {
 
-	private ArrayList<Vertex> vertices;
+	private HashMap<String,Vertex> vertices;
 
 	public Graph() {
 		super();
+	}
+
+	// inserts an edge between 2 specified vertices
+	public void insertEdge(String strVertex1UniqueID, String strVertex2UniqueID, String strEdgeUniqueID,
+			String strEdgeData, int nEdgeCost) throws GraphException {
+		Vertex vertexA = this.vertices.get(strVertex1UniqueID);
+		Vertex vertexB = this.vertices.get(strVertex2UniqueID);
+		Edge edge= new Edge(new StringBuffer(strEdgeUniqueID) , new StringBuffer(strEdgeData),nEdgeCost, vertexA,vertexB);
+
 	}
 }
